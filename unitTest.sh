db5c5d68b2179795925cf5193dd347d127017b4b
zen <zhangyiming748@gmail.com>
2020-08-23 21:05:09 +0800

    排除了部分报错的重复文件

---
+++ unitTest.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+
+# line广告
+adb shell rm -rf /storage/sdcard0/Android/data/jp.naver.line.android/channel/nativebar/linepoll
+adb shell touch /storage/sdcard0/Android/data/jp.naver.line.android/channel/nativebar/linepoll
