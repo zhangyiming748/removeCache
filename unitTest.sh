13b85b9f5790ea7e9e4f18e0f678c96b56acd7f1
zen <zhangyiming748@gmail.com>
2020-09-02 22:12:12 +0800

    2020年9月2日由于之前的误操作重新更新

--- unitTest.sh
+++ unitTest.sh
@@ -1,20 +1,4 @@
 #! /bin/bash
 
-# QQ日志
-adb shell rm -rf /storage/sdcard0/tencent/msflogs
-adb shell touch /storage/sdcard0/tencent/msflogs
-# QQ动态摄影素材
-adb shell rm -rf /storage/sdcard0/tencent/MobileQQ/ptv_template
-adb shell touch /storage/sdcard0/tencent/MobileQQ/ptv_template
-# QQ红包缓存
-adb shell rm -rf /storage/sdcard0/tencent/MobileQQ/RedPacket
-adb shell touch /storage/sdcard0/tencent/MobileQQ/RedPacket
-# QQ音乐本地播放器
-adb shell rm -rf /storage/sdcard0/qqmusiclocalplayer
-adb shell touch /storage/sdcard0/qqmusiclocalplayer
-# QQ故事
-adb shell rm -rf /storage/sdcard0/qqstory
-adb shell touch /storage/sdcard0/qqstory
-# QQ内置浏览器
-adb shell rm -rf /storage/sdcard0/QQBrowser
-adb shell touch /storage/sdcard0/QQBrowser
+adb shell rm -rf /storage/sdcard0/Android/data/com.tencent.mm/cache
+adb shell touch /storage/sdcard0/Android/data/com.tencent.mm/cache
